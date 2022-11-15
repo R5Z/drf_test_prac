b81ce7c1613e623be621559d4bbc864c234e419a
R5Z Yoon <asaucybloom@gmail.com>
2022-11-15 09:34:59 +0900

    Add: User signin Test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.urls import reverse # url에 있는 네임값으로 테스트 가능. 향후 url이 바뀌더라도 네임값을 사용하기 때문에 오류가 나거나 꼬일 위험이 적다
+from rest_framework.test import APITestCase
+from rest_framework import status
+from users.models import User, UserManager
 
-# Create your tests here.
+
+class UserRegistrationTest(APITestCase): 
+    def test_registration(self):
+        url = reverse("userview")
+        user_data = {
+            "username": "testsign",
+            "email": "te@st.com",
+            "fullname": "rose",
+            "password": "wkdal"
+        }
+        response = self.client.post(url, user_data)
+        self.assertEqual(response.json(), {
+            "message": "가입 완료!!"
+        })
